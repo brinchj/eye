61d5e77282b654109f62abf0ccdd650325b5b03d
Johan Brinch <brinchj@gmail.com>
2010-10-05 20:02:07 +0200

    Add scrollbar logger

--- gui/gui.py
+++ gui/gui.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
-import time
+import time, thread
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QObject
 
-from form import Ui_MainWindow
+from models.main import Ui_MainWindow
+
+class ScrollLogger(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+        self.last = 0
+        self.log  = []
+        self.last_flush = time.time()
+        thread.start_new_thread(self.flusher, ())
+    def eventFilter(self, obj, e):
+        if self.last != obj.value():
+            self.last = obj.value()
+            self.log.append( (time.time(), self.last) )
+        return False
+    def flusher(self):
+        while True:
+            self.flush()
+            time.sleep(1)
+    def flush(self):
+        log, self.log = list(self.log), []
+        if log:
+            file('session.scrl.log','a').write(
+                '\n'.join( '%f    %i' % (t, pos) for (t,pos) in log)+'\n')
 
 def main():
-    global s, widget, app
+    global s, widget, app, f
     app = QtGui.QApplication([])
     widget = QtGui.QMainWindow()
     form = Ui_MainWindow()
@@ -17,7 +40,9 @@ def main():
     s.setMinimumWidth(25)
     s.sizeHint = lambda : QtCore.QSize(25, 41)
     form.txtSource.setVerticalScrollBar(s)
-    form.txtAssignment.appendHtml(file('0.txt.html').read())
+    form.txtAssignment.appendHtml(file('data/python.html').read())
+    f = ScrollLogger()
+    s.installEventFilter(f)
     widget.show()
     time.sleep(.5)
     s.setValue(0)
