fd72b7d9d3a0ac2b0eb4b89961fbe3e6d471743a
Johan Brinch <brinchj@gmail.com>
2010-10-06 14:46:45 +0200

    Count keywords used in Python

---
+++ opgs/1/py/kw.py
@@ -0,0 +1,15 @@
+import sys,keyword,re
+
+words = keyword.kwlist
+
+code = file(sys.argv[1]).read()
+
+code = '\n'.join( l for l in code.split('\n')
+                  if len(l.strip()) == 0 or
+                  not l.strip()[0] in ('#','"',"'") )
+
+
+sort = sorted(( (w, len(re.findall(r'[^\w]%s[^\w]' % w, code))) for w in words),
+                key=lambda p: -p[1])
+
+print '\n'.join('%s: %s' % (w.rjust(10), c) for w,c in sort)
