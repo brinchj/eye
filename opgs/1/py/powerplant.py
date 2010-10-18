73cc5e86b2077af819dc38def8b82b530e4b4213
Johan Brinch <brinchj@gmail.com>
2010-10-18 10:38:57 +0200

    Newest

--- opgs/1/py/powerplant.py
+++ opgs/1/py/powerplant.py
@@ -11,12 +11,10 @@ instruments = [
 
 """ Manipulate data """
 def add(ins, name, value):
-    """ Add new instrument """
     # return
-    return ins + [ [name, value, True] ]
+    return [ [name, value, True] ] + ins
 
 def has(ins, name):
-    """ Check if instrument exists """
     # for, if, return
     for i in ins:
         if i[0] == name:
@@ -24,26 +22,24 @@ def has(ins, name):
     return False
 
 def rem(ins, name):
-    """ Remove instrument """
     # if, for, return
     if len(ins) == 0:
-        return None
+        return []
     ins = list(ins)
     for i in range(len(ins)):
         if ins[i][0] == name:
             del ins[i]
             return ins
-    return None
+    return ins
 
 def update(ins, name, value):
     # function call in function call
     if len(ins) == 0 or not has(ins, name):
-        return None
+        return ins
     return add(rem(ins, name), name, value)
 
-""" Report failed systems """
+""" Check for failures """
 def first_failed(ins):
-    """ Find first failed instriment """
     # for, if, not, return
     i = 0
     while i < len(ins):
@@ -73,34 +69,38 @@ def diff(ins0, ins1):
         for elem1 in ins1:
             if elem0 == elem1:
                 change = False
-        if change:
+        if change == True:
             elems.append(elem0)
     return elems
 
 def diff_all(ins0, ins1):
     # return
-    return str(diff(ins0, ins1)) + ' => ' + str(diff(ins1, ins0))
+    s  = str(diff(ins0, ins1)) + ' => '
+    s += str(diff(ins1, ins0))
+    return s
 
 """ Print a status report """
 def print_status(ins):
     # for, if, else
-    print '='*64
-    print 'Name:', 'Value:', 'Operating:'
+    print '===='
+    print 'Name: Value: Operating:'
     for elem in ins:
         oper = ''
-        if elem[2]:
+        if elem[2] == True:
             oper = 'Yes'
         else:
             oper = 'No'
-        print elem[0] + ', ' + str(elem[1]) + ', ' + oper
+        s = elem[0] + ', ' + str(elem[1]) + ', ' + oper
+        print s
     print ''
     print 'High:', too_high(ins)
     print ''
     print 'Failed:', first_failed(ins)
     print '='*64
 
+""" CLI for manipulating data """
 def updater(ins):
-    # while, if, elif, else, return
+    # while, if, elif, else, return, raw_input
     while True:
         print_status(ins)
         print ''
@@ -116,17 +116,17 @@ def updater(ins):
                 print 'No value.'
                 ins_new = ins
             else:
-                ins_new = add(ins, name, value)
+                ins_new = add(ins, name, int(value))
         else:
             value = raw_input('CHANGE value=')
             if value == 'REM':
                 print 'REMOVE'
                 ins_new = rem(ins, name)
-            elif len(value) > 0:
-                ins_new = update(ins, name, int(value))
-            else:
+            elif len(value) == 0:
                 print 'No value.'
                 ins_new = ins
+            else:
+                ins_new = update(ins, name, int(value))
         print diff_all(ins, ins_new)
         print ''
         ins = ins_new
