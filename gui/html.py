cacfd959f0eb26fbcdc9e69793f109c7183fba8d
Johan Brinch <brinchj@gmail.com>
2010-10-18 18:07:25 +0200

    1 space indentation only

--- gui/html.py
+++ gui/html.py
@@ -3,8 +3,9 @@ import re
 def add_lines_to_html(html):
     match = re.match(r'(.*)<pre>(.*)</pre>(.*)', html, re.DOTALL)
     header, pre, footer = match.groups()
-    lines = '\n'.join( '%3i  %s' % (i,l)
-                       for i,l in enumerate(pre.strip().split('\n')) )
+    print pre
+    lines = '\n'.join( '%3i%s' % (i,l)
+                       for i,l in enumerate((' '+(pre.strip('\n'))).split('\n')) )
     return '%s<pre>%s</pre>%s' % (header, lines, footer)
 
 def code_process_html(path):
